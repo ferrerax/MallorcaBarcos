b6993f98ab0474d0c523d0a7233efd25fc720f85
carla <carla@MacBook-Air-de-carla.local>
2020-08-29 16:12:40 +0200

    amb l'output ben formatat

--- barcos.py
+++ barcos.py
@@ -32,29 +32,32 @@ def obtepags():
 	time.sleep(2)
 	for e in elem:
 		if e.text == "I ACCEPT":
-			print("[X]	Acceptant Cookies...")
+			#print("[X]	Acceptant Cookies...")
 			e.click()
 	time.sleep(4)
 	elems = driver.find_elements_by_tag_name("p")
 	for e in elems:
 		if "of" in e.text:
 			num = int(e.text.split()[1])
-			print ("Tenim " + str(num) + " pagines")
+			print ("Tenim " + str(num*20) + " vaixells al port de Palma")
 	return num
 
-
-for p in xrange(0,obtepags()-1): #Tenim obtepags numero de pagines
+num_pag=obtepags()
+num_vaixells=num_pag*20
+nbarco=0
+print("[+] BUSCADOR DE VAIXELLS AL PORT DE PALMA DE MALLORCA")
+for p in xrange(0,num_pag-1): #Tenim obtepags numero de pagines
 	driver.get(url)
 	
 # Acceptar les cookies
 	elem = driver.find_elements_by_class_name("qc-cmp-button")
 	for e in elem:
 		if e.text == "I ACCEPT":
-			print("[X]	Acceptant Cookies...")
+		#	print("[X]	Acceptant Cookies...")
 			e.click()
 #Itero tots els botons fins que trobo el de canviar la pàgina. No ho faig a la primera pagina
 	elem = driver.find_elements_by_tag_name("button")
-	print(str(len(elem)))
+	#print(str(len(elem)))
 	for e in elem:
 	#	print(e)
 		#if not first and "Next page" in e.get_attribute("title"):
@@ -63,11 +66,11 @@ for p in xrange(0,obtepags()-1): #Tenim obtepags numero de pagines
 		#	first=False
 		#print (e.get_attribute("title"))	
 		if "Next page" in e.get_attribute("title"):
-			print("[X]	Canviant a la  pagina... " + str(p+1))
+	#		print("[X]	Canviant a la  pagina... " + str(p+1))
 			for i in range(0,p):	
 				e.click()
 				time.sleep(2)
-				print("Estic a la pàgina " + str(p+1))
+	#			print("Estic a la pàgina " + str(p+1))
 
 
 
@@ -77,21 +80,26 @@ for p in xrange(0,obtepags()-1): #Tenim obtepags numero de pagines
 	imos=[]
 	dlinks=[]
 	llarg=[]
-	print("He obtingut " + str(len(elem)) + " elements en aquesta pàgina") #Mostro la quantitat de barcos que tinc
+	#print("He obtingut " + str(len(elem)) + " elements en aquesta pàgina") #Mostro la quantitat de barcos que tinc
 	for e in elem:
 		if "Show Details For:" in e.get_attribute("title"):
 			noms.append(e.text) #Em quedo el nom del barco
 			dlinks.append(e.get_attribute("href")) #Em quedo el link del barco
 			#print(e.text)		# Mostro el nom del barco
 	
-	print("[X]	"+str(len(dlinks))+" vaixells trobats")#Mostro el numero de barcos q he trobat
+	#print("[X]	"+str(len(dlinks))+" vaixells trobats")#Mostro el numero de barcos q he trobat
+	it = 0
 	for l in dlinks:  # per cada link que he trobat a la pàgina
-		print("Investigo " + l)
+		print("Processant vaixell " + str(nbarco+1) + " de " + str(num_vaixells))
+		print("Nom del vaixell: " + noms[it])
+		it += 1
+		nbarco += 1
+	#	print("Investigo " + l)
 		driver.get(l)
 		elem = driver.find_elements_by_class_name("qc-cmp-button")
 		for e in elem:
 			if e != None and e.text == "I ACCEPT":
-				print("[X]	Acceptant Cookies...")
+				#print("[X]	Acceptant Cookies...")
 				e.click()
 		h = driver.find_element_by_tag_name("body") 
 		for i in xrange(0,10):	# Faig scroll molt cutre
@@ -103,12 +111,12 @@ for p in xrange(0,obtepags()-1): #Tenim obtepags numero de pagines
 			info = driver.find_element_by_id("imo")
 			m = re.search(r'(?<=: )[0-9\-]+', info.text)
 			imo = m.group(0)
-			print("			IMO: " + str(imo))
+			#print("			IMO: " + str(imo))
 			imos.append(imo)
 			info = driver.find_element_by_id("lengthOverallBreadthExtreme")
 			m = re.search(r'(?<=: )[0-9\.]*', info.text)
 			llargi = m.group(0)
-			print("			Llarg: " + str(llargi))
+			#print("			Llarg: " + str(llargi))
 			llarg.append(llargi)
 		except NoSuchElementException:
 			imos.append(None)
@@ -117,7 +125,7 @@ for p in xrange(0,obtepags()-1): #Tenim obtepags numero de pagines
 			
 print("[+] FORMATANT LA SOTIDA")
 with open("./stalin.csv","a+") as fd:
-	print(fd)
+	#print(fd)
 	csv_columns = ["Nom","IMO","Llargada en metres"]
 	barcos = formataSortida(noms,imos,llarg)
 	writer = csv.DictWriter(fd,fieldnames=csv_columns)
@@ -129,4 +137,4 @@ with open("./stalin.csv","a+") as fd:
 #		fd.write(noms[i]+";"+l+";https://www.google.com/search?q=intext:\""+noms[i]+"\" intext:"+imos[i])
 #		i+=1
 driver.close()
-print("[+] Acabat")
+#print("[+] Acabat")
